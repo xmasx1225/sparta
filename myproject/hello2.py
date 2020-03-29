0cedfece898cc56486c8d2c87d7f7bbdf77e32ac
xmasx <xmasx1225@gmail.com>
2020-03-29 23:02:39 +0900

    homework1

---
+++ myproject/hello2.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+movies = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+# movies (tr들) 의 반복문을 돌리기
+rank = 1
+for movie in movies:
+    # movie 안에 a 가 있으면,
+    a_tag = movie.select_one('td.info > a.title.ellipsis')
+    if a_tag is not None:
+        title = a_tag.text.strip()
+        star = movie.select_one('td.info > a.artist.ellipsis').text
+        print(rank,title,star)
+        rank += 1
